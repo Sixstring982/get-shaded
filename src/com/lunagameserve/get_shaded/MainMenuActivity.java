4e3d1cd690220832f0980063ac349a1134c38b1f
sixstring982 <sixstring982@gmail.com>
2015-04-09 17:31:18 -0600

    Initial Android Project

---
+++ src/com/lunagameserve/get_shaded/MainMenuActivity.java
@@ -0,0 +1,15 @@
+package com.lunagameserve.get_shaded;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenuActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

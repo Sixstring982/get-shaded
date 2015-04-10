75f2fce4f8d2273db56424535fa8d1ec5f0ed449
sixstring982 <sixstring982@gmail.com>
2015-04-09 18:11:05 -0600

    Maps integration ready to go

--- src/com/lunagameserve/get_shaded/MainMenuActivity.java
+++ src/com/lunagameserve/get_shaded/MainMenuActivity.java
@@ -1,7 +1,9 @@
 package com.lunagameserve.get_shaded;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends Activity {
     /**
@@ -11,5 +13,10 @@ public class MainMenuActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void pushMapActivity(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 }
---
+++ src/com/lunagameserve/get_shaded/MapActivity.java
@@ -0,0 +1,16 @@
+package com.lunagameserve.get_shaded;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sixstring982 on 4/9/15.
+ */
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_layout);
+    }
+}
